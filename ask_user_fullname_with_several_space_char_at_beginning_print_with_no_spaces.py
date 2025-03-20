66fa6c9e3b15e3ef87e586c9b8bf9770e2660f21
Josh Obatay <joshobatay2005@gmail.com>
2025-03-20 13:48:04 +0800

    added input

--- ask_user_fullname_with_several_space_char_at_beginning_print_with_no_spaces.py
+++ ask_user_fullname_with_several_space_char_at_beginning_print_with_no_spaces.py
@@ -1,6 +1,7 @@
 # ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
 
 # ask user's fullname 
+full_name = input("Enter your full name: ")
 
 # process text into cleaner version
 
