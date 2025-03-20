c9d537a92e26da297aff15f8f36d107404917b4b
Josh Obatay <joshobatay2005@gmail.com>
2025-03-20 13:45:52 +0800

    pseudocode

---
+++ ask_user_fullname_with_several_space_char_at_beginning_print_with_no_spaces.py
@@ -0,0 +1,7 @@
+# ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+
+# ask user's fullname 
+
+# process text into cleaner version
+
+# output fullname
