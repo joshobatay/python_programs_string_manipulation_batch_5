0595e41903b29dab200b887996228da0213182f9
Josh Obatay <joshobatay2005@gmail.com>
2025-03-20 15:10:52 +0800

    pseudocode

---
+++ ask_user_fullname_in_incorrect_case_print_in_proper_case.py
@@ -0,0 +1,7 @@
+# Ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+
+# Ask user their full name
+
+# Process entered full name to proper casing
+
+# Output full name in proper casing
