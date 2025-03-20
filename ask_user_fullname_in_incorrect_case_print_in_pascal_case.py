98fc019f38c1d3bc8f1ee055adc6d231f82160fb
Josh Obatay <joshobatay2005@gmail.com>
2025-03-20 16:11:11 +0800

    added process

--- ask_user_fullname_in_incorrect_case_print_in_pascal_case.py
+++ ask_user_fullname_in_incorrect_case_print_in_pascal_case.py
@@ -4,5 +4,6 @@
 entered_full_name = input("Enter full name: ")
 
 # Process full name in incorrect casing to pascal case
+full_name_pascal_case = entered_full_name.title().replace(" ", "") # title() for proper casing, replace() for removing spaces
 
 # Output full name to pascal case
