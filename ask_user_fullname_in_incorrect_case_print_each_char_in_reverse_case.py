fb55008bcf6a958cc625ea353b5447f09f6bdd35
Josh Obatay <joshobatay2005@gmail.com>
2025-03-20 15:21:28 +0800

    added input

--- ask_user_fullname_in_incorrect_case_print_each_char_in_reverse_case.py
+++ ask_user_fullname_in_incorrect_case_print_each_char_in_reverse_case.py
@@ -1,7 +1,9 @@
 # Ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
 
 # Ask user their full name in incorrect casing
+entered_full_name = input("Enter full name: ")
 
 # Process entered full name to reverse casing
+
 
 # Output entered full name in reverse case
