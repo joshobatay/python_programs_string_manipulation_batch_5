6985e842c02edbc5237e6db530afb4cc8bb40c62
Josh Obatay <joshobatay2005@gmail.com>
2025-03-20 13:54:20 +0800

    added process

--- ask_user_fullname_with_several_space_char_at_beginning_print_with_no_spaces.py
+++ ask_user_fullname_with_several_space_char_at_beginning_print_with_no_spaces.py
@@ -4,5 +4,6 @@
 full_name = input("Enter your full name: ")
 
 # process text into cleaner version
+cleaned_full_name = full_name.strip() # .strip removes leading and trailing spaces
 
 # output fullname
