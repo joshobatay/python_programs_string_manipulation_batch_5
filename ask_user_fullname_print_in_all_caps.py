fe06fd1e28bc1c4b9a6e8e95c52891ea4281b436
Josh Obatay <joshobatay2005@gmail.com>
2025-03-20 14:32:56 +0800

    added process

--- ask_user_fullname_print_in_all_caps.py
+++ ask_user_fullname_print_in_all_caps.py
@@ -2,6 +2,8 @@
 
 # Ask user their full name
 entered_full_name = input("Enter full name: ")
+
 # Process full name to all caps
+full_name_all_caps = entered_full_name.upper() # .upper converts text to all capitals
 
 # Output full name in all caps
