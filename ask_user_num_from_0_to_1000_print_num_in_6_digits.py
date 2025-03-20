70865c9af633b87189f4a7f2cdf891641a7ad629
Josh Obatay <joshobatay2005@gmail.com>
2025-03-20 14:14:14 +0800

    added input

--- ask_user_num_from_0_to_1000_print_num_in_6_digits.py
+++ ask_user_num_from_0_to_1000_print_num_in_6_digits.py
@@ -1,7 +1,9 @@
-# ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
+# Ask the user to input a number (0-1000), print the number in 6 digit format. 
+# Add zeros at the beginning to complete the 6 digit.
 
-# ask user to input number (0 - 1000)
+# Ask user to input number (0 - 1000)
+entered_number = int(input('Enter number: '))
 
-# process entered number
+# Process entered number
 
-# output number as 6 digits
+# Output number as 6 digits
