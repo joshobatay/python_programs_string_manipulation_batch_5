48bcb26d07e95b45ecbe67423426433034aa7042
Josh Obatay <joshobatay2005@gmail.com>
2025-03-20 15:49:38 +0800

    pseudocode

---
+++ ask_user_correct_statement_print_number_of_words.py
@@ -0,0 +1,7 @@
+# Ask the user to input a complete statement. Print the number of words in the input
+
+# Ask user complete statement
+
+# Process number of words entered
+
+# Output number of words
