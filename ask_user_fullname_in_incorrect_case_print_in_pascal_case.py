e13f5e8ee3931e2f887f7161140828bf74fb9869
Josh Obatay <joshobatay2005@gmail.com>
2025-03-20 16:07:20 +0800

    pseudocode

---
+++ ask_user_fullname_in_incorrect_case_print_in_pascal_case.py
@@ -0,0 +1,7 @@
+# Ask the user to input their fullname in incorrect casing. Print the input in pascal case.
+
+# Ask user full name in incorrect casing
+
+# Process full name in incorrect casing to pascal case
+
+# Output full name to pascal case
