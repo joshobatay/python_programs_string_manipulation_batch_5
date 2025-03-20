74957a0245ca34aa5e5cfcc87e181c7cb88d2429
Josh Obatay <joshobatay2005@gmail.com>
2025-03-20 15:12:30 +0800

    added input

--- ask_user_fullname_in_incorrect_case_print_in_proper_case.py
+++ ask_user_fullname_in_incorrect_case_print_in_proper_case.py
@@ -1,6 +1,7 @@
 # Ask the user to input their fullname in incorrect casing. Print the input in proper casing.
 
 # Ask user their full name
+entered_full_name = input('Enter full name: ')
 
 # Process entered full name to proper casing
 
