6bd25b5dbfcf726594e91d9c3c53512df5f372ba
Josh Obatay <joshobatay2005@gmail.com>
2025-03-20 16:16:22 +0800

    pseudocode

---
+++ ask_user_fullname_in_incorrect_case_print_in_snake_case.py
@@ -0,0 +1,7 @@
+# Ask the user to input their fullname in incorrect casing. Print the input in snake case.
+
+# Ask user their full name in incorrect spacing
+
+# Process entered full name in incorrect spacing to snake case
+
+# Output entered full name to snake case
