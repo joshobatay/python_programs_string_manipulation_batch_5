9bf8f249741f2dd80a13cc35dce45c9c2acf93d4
Josh Obatay <joshobatay2005@gmail.com>
2025-03-20 16:00:50 +0800

    pseudocode

---
+++ ask_user_fullname_print_num_of_char.py
@@ -0,0 +1,7 @@
+# Ask the user to input their fullname. Print the number of characters in the input.
+
+# Ask user their full name
+
+# Process entered full name to count characters
+
+# Output counted characters
