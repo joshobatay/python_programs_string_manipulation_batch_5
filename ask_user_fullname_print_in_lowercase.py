9c44d8df4e1ce2df80392e1da698c262844a80d6
Josh Obatay <joshobatay2005@gmail.com>
2025-03-20 14:47:16 +0800

    added input

--- ask_user_fullname_print_in_lowercase.py
+++ ask_user_fullname_print_in_lowercase.py
@@ -1,8 +1,10 @@
 # Ask the user to input their fullname. Print the input in all lower case.
 
 # Ask user their full name
+entered_full_name = input("Enter full name: ")
 
 # Process entered full name to lower case
+
 
 # Output full name in lower case
 
