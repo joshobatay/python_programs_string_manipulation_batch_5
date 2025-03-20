524da8f8e82286f0700509ca06605421646cdb3a
Josh Obatay <joshobatay2005@gmail.com>
2025-03-20 14:46:10 +0800

    pseudocode

---
+++ ask_user_fullname_print_in_lowercase.py
@@ -0,0 +1,8 @@
+# Ask the user to input their fullname. Print the input in all lower case.
+
+# Ask user their full name
+
+# Process entered full name to lower case
+
+# Output full name in lower case
+
