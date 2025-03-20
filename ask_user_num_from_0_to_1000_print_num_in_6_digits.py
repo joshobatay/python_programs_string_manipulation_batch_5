5878337f9fa4fd25b2ff441dfadcdb831b262bf9
Josh Obatay <joshobatay2005@gmail.com>
2025-03-20 14:05:46 +0800

    pseudocode

---
+++ ask_user_num_from_0_to_1000_print_num_in_6_digits.py
@@ -0,0 +1,7 @@
+# ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
+
+# ask user to input number (0 - 1000)
+
+# process entered number
+
+# output number as 6 digits
